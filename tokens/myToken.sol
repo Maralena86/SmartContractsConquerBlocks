4b149f2437cdd098122e84fb086e5bbab6713b1a
Maralena86 <mari16cobain@msn.com>
2023-05-30 18:27:07 +0200

    actu contract

--- tokens/myToken.sol
+++ tokens/myToken.sol
@@ -1,17 +1,27 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.14;
+
 import "./ERC20.sol";
 
-contract MyToken is ERC20
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MyToken is ERC20, AccessControl
 {
-    address owner;
-    constructor() ERC20("MyminiToken", "MnT"){
-        owner = msg.sender;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() ERC20("MyminiToken", "MnT", 10000000000000000000000){
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
     }
 
-    function mintToken () public {
-        require(msg.sender ==owner, "Is not the owner");
+    function mintToken () public onlyRole(MINTER_ROLE) {   
         _mint(msg.sender, 1000000000000000000000);
+    }
+
+    function burnTokens()  public onlyRole(BURNER_ROLE){
+        _burn(msg.sender, 1000000000000000000000);
     }
 
 }
